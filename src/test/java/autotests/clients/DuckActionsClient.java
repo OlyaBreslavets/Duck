21355e399e01f9e7ee21b8b8e9c1ca0aacef9f3b
Olya Breslavets <breslavets.olya@gmail.com>
2023-07-18 15:52:34 +0700

    Исправления по замечаниям: 1.	Поскольку это get запрос (метод duckFly), который не предполагает передачу тела, то и contentType указывать нет смысла. Соответственно и message() тоже можно убрать. Посмотри на все методы и убери message и contentType, где они не нужны. 2.	Параметр heigh лучше передать с типом String, так как тело мы в методе duckCreate передаем просто строкой.

--- src/test/java/autotests/clients/DuckActionsClient.java
+++ src/test/java/autotests/clients/DuckActionsClient.java
@@ -20,18 +20,14 @@ public class DuckActionsClient extends TestNGCitrusSpringSupport {
         runner.$(http().client(duckService)
                 .send()
                 .get("/api/duck/action/fly")
-                .queryParam("id", id)
-                .message()
-                .contentType(MediaType.APPLICATION_JSON_VALUE));
+                .queryParam("id", id));
     }
 
     public void duckProperties(TestCaseRunner runner, String id) {
         runner.$(http().client(duckService)
                 .send()
                 .get("/api/duck/action/properties")
-                .queryParam("id", id)
-                .message()
-                .contentType(MediaType.APPLICATION_JSON_VALUE));
+                .queryParam("id", id));
     }
 
     public void duckQuack(TestCaseRunner runner, String id, String repetitionCount, String soundCount){
@@ -40,17 +36,13 @@ public class DuckActionsClient extends TestNGCitrusSpringSupport {
                 .get("/api/duck/action/quack")
                 .queryParam("id", id)
                 .queryParam("repetitionCount", repetitionCount)
-                .queryParam("soundCount", soundCount)
-                .message()
-                .contentType(MediaType.APPLICATION_JSON_VALUE));
+                .queryParam("soundCount", soundCount));
     }
 
     public void duckSwim(TestCaseRunner runner, String id) {
         runner.$(http().client(duckService)
                 .send()
                 .get("/api/duck/action/swim")
-                .queryParam("id", id)
-                .message()
-                .contentType(MediaType.APPLICATION_JSON_VALUE));
+                .queryParam("id", id));
     }
 }
